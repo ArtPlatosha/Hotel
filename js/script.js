ae475d9ea77cb02d52983161303a5754d20d073d
ArtPlatosha <zipex98@gmail.com>
2023-04-16 10:34:59 +0300

    commit: 15th commit
    adding room-slider (JS)

--- js/script.js
+++ js/script.js
@@ -11,3 +11,29 @@ var swiper = new Swiper('.home-slider', {
 		prevEl: '.swiper-button-prev'
 	}
 });
+
+var swiper = new Swiper('.room-slider', {
+	spaceBetween: 20,
+	grabCursor: true,
+	loop:true,
+	centeredSlides:true,
+	autoplay: {
+		delay: 7500,
+		disableOnInteraction: false,
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+		},
+		768: {
+			slidesPerView: 2,
+		},
+		991: {
+			slidesPerView: 3,
+		},
+	},
+});
