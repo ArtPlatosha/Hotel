350de7f6c963d7d5ace190d504ae3d20558c5668
ArtPlatosha <zipex98@gmail.com>
2023-04-18 10:31:21 +0300

    commit: 31st commit
    adding media-source (991px and 768px) + JS burger

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+let menu =document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () => {
+	menu.classList.toggle('fa-times');
+	navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+	menu.classList.remove('fa-times');
+	navbar.classList.remove('active');
+}
+
 var swiper = new Swiper('.home-slider', {
 	grabCursor: true,
 	loop:true,
