597c40273b234c38cfc0d55babce488e68f82f82
ArtPlatosha <zipex98@gmail.com>
2023-04-18 08:49:18 +0300

    commit: 23th commit
    adding review-section (JS)

--- js/script.js
+++ js/script.js
@@ -63,3 +63,18 @@ var swiper = new Swiper('.gallery-slider', {
 		},
 	},
 });
+
+var swiper = new Swiper('.review-slider', {
+	spaceBetween: 10,
+	grabCursor: true,
+	loop:true,
+	centeredSlides:true,
+	autoplay: {
+		delay: 7500,
+		disableOnInteraction: false,
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+});
